6bfd5516c3e15d3ad98c840e4ac330b40b2aa6c8
Kostya Sproge <kostya.sproge@gmail.com>
2018-03-12 18:36:39 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp         = require('gulp');
+var minifyCss    = require('gulp-clean-css');
+var sass         = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+// var sourcemaps   = require('gulp-sourcemaps');
+var notify       = require("gulp-notify");
+var uglifyJs     = require('gulp-uglify');
+var concatJs     = require('gulp-concat');
+
+
+var adminLibsJsArr = [
+    'web/js/lib/jquery-2.1.4.min.js',
+    'web/js/lib/modules/collapse.js',
+    'web/js/lib/onepage.js',
+];
+
+
+gulp.task('adminCss', function(){
+   return gulp.src('web/scss/admin/admin.scss')
+       .pipe(sass().on('error', notify.onError()))
+       .pipe(autoprefixer('last 4 versions'))
+       .pipe(minifyCss())
+       .pipe(gulp.dest('web/css'))
+       .pipe(notify('Done admin css!'));
+});
+gulp.task('adminLibsJs', function() {
+    return gulp.src(adminLibsJsArr)
+        .pipe(concatJs('adminLibs.js'))
+        .pipe(uglifyJs().on('error', notify.onError()))
+        .pipe(gulp.dest('web/js/dist'))
+        .pipe(notify('Done adminJs!'));
+});
+
+
+gulp.task('watcher', function(){
+  gulp.watch('web/scss/admin/**/*.scss', ['adminCss']);
+});
+
+//____________________________
+gulp.task('default', [
+    'watcher',
+    'adminCss',
+    'adminLibsJs',
+]);
+
+
